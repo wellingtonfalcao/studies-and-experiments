25c9e410c5a70b7041ddce1c1962acc3de90ddfb
Wellington Falcão <wellingtonfalcao@gmail.com>
2025-08-06 20:17:52 -0300

    Update: mapped jpa entities and added lombok for getters, constructors, and equalsandhashcode

---
+++ spring-boot/clinica-medica/src/main/java/br/com/clinica/api/medico/Medico.java
@@ -0,0 +1,29 @@
+package br.com.clinica.api.medico;
+
+import br.com.clinica.api.endereco.Endereco;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "medicos")
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Medico {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String crm;
+
+    @Enumerated(EnumType.STRING)
+    private Especialidade especialidade;
+
+    @Embedded
+    private Endereco endereco;
+}
