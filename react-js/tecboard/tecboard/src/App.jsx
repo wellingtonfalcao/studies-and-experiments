8a3bb338eca7b083741b82b125e818e08cdb4c19
Wellington Falcão <wellingtonfalcao@gmail.com>
2026-01-30 22:22:20 -0300

    New react project called tecboard

---
+++ react-js/tecboard/tecboard/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        teste
+        <img src="" alt=""></img>
+      </header>
+    </main>
+  )
+}
+
+export default App
