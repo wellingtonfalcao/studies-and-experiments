0db90ce20baef69e5d8d115abe0bbe122dfaf441
Wellington Falcão <wellingtonfalcao@gmail.com>
2026-02-09 15:18:14 -0300

    Update: C Course at 37 class of Programa seu Futuro: https://www.youtube.com/watch?v=7J95kCTYLEc&list=PLqJK4Oyr5WSjjEQCKkX6oXFORZX7ro3DA&index=39

--- c/hello-world/teste/main.c
+++ c/hello-world/teste/main.c
@@ -1,13 +1,135 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
+    setlocale(LC_ALL, "Portuguese");
+
+
+
+ /*
+    printf("---------------------------------------------------------------------\n");
+    printf("---------------------------EXERCICIO 02 -----------------------------\n");
+    printf("---------------------------------------------------------------------\n");
+
+    int a = 10;
+    int b = 20;
+    int carry;
+
+    printf("Esses são os valores iniciais: \n A = %d, B = %d\n\n", a, b);
+
+    carry = a;
+    a = b;
+    b = carry;
+
+    printf("Esses são os valores invertidos: \n A = %d(B), B = %d(A)\n\n", a, b);
+
+    printf("Digite um valor para A: ");
+    scanf("%d", &a);
+
+    printf("Digite um valor para B: ");
+    scanf("%d", &b);
+
+    printf("Esses são os valores finais para: \n A = %d, B = %d\n\n", a, b);
+
+
+    printf("---------------------------------------------------------------------\n");
+    printf("---------------------------EXERCICIO 03 -----------------------------\n");
+    printf("---------------------------------------------------------------------\n");
+
+    int c = 10;
+    int d = 20;
+
+    c = c + d;
+    d = c - d;
+    c = c - d;
+
+    printf("Esses são os valores finais para: \n C = %d, D = %d\n\n", c, d);
+
+
+    printf("---------------------------------------------------------------------\n");
+    printf("---------------------------EXERCICIO 04 -----------------------------\n");
+    printf("---------------------------------------------------------------------\n");
+
+    float conta;
+    int qtd_pessoas;
+    float gorjeta = 0.1;
+
+
+    printf("Digite o valor da conta e a quantidade de pessoas: ");
+    scanf("%f%d", &conta, &qtd_pessoas);
+
+    float calculo = (conta * gorjeta) + conta;
+    float individual = calculo / qtd_pessoas;
+
+    printf("O valor total da conta é de R$ %.2f e o valor por pessoa é de R$ %.2f", calculo, individual);
+
+
+
+    printf("---------------------------------------------------------------------\n");
+    printf("---------------------------EXERCICIO 05 -----------------------------\n");
+    printf("---------------------------------------------------------------------\n");
+
+    float diaria = 45.0;
+    float ir = 0.08;
+    int dias_trab;
+
+    printf("A diaria do encanador é de R$ %.2f. Digite a quantidade de dias trabalhados por ele: ", diaria);
+    scanf("%d", &dias_trab);
+
+    float valor_bruto = diaria * dias_trab;
+    float valor_liquido = valor_bruto - (valor_bruto * ir);
+
+    printf("O valor bruto total é de R$ %.2f e o valor líquido é de R$ %.2f", valor_bruto, valor_liquido);
+
+
+
+    printf("---------------------------------------------------------------------\n");
+    printf("---------------------------EXERCICIO 06 -----------------------------\n");
+    printf("---------------------------------------------------------------------\n");
+
+    float cotacao_dolar = 1.0, cotacao_real = 2, valor_real, valor_dolar;
+
+    printf("Digite o valor em reais que deseja converter: R$ ");
+    scanf("%f", &valor_real);
+
+    valor_dolar = (valor_real / cotacao_real) * cotacao_dolar;
+
+    printf("O valor total de R$ %.2f convertido em dólar é de $ %.2f\n\n", valor_real, valor_dolar);
+
+    printf("---------------------------------------------------------------------\n");
+    printf("---------------------------EXERCICIO 07 -----------------------------\n");
+    printf("---------------------------------------------------------------------\n");
+
+    int entrada_qnt_segundos, horas, minutos, segundos;
+
+    printf("Digite a quantidade de segundos e lhe retonarei em HH:MM:SS: ");
+    scanf("%d", &entrada_qnt_segundos );
+
+    horas = entrada_qnt_segundos / 3600;
+    minutos = (entrada_qnt_segundos / 60) % 60;
+    segundos = ((entrada_qnt_segundos % 60) % 60 ) % 60;
+
+    printf("\nTotal de: Horas: %d, Minutos: %d, Segundos: %d\n\n", horas, minutos, segundos);
+    */
+
+
+    /*
+    setlocale(LC_ALL,"Portuguese");
+    printf("A codificação é: %s\n", setlocale(LC_ALL,"Portuguese"));
+    printf("Arrebentação é muito legal\n");
+    printf("%c", 7);
+
+
+    int a = 10, b = 20;
+
+    printf("Resto da divisao inteira: %d\n", b % a);
+
     int a = 10, b = 20;
 
     printf("\nDivisao de %d por %d e: %f\n", a, b, (float)a / b);
 
-    /*
     printf("\nSoma: %d\n", 50 + 50);
     printf("\nSubtracao: %d\n", 50 - 20);
     printf("\nMultiplicacao: %d\n", 50 * 4);
